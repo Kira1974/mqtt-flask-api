51c5b26ac9d51fb0c6a0c2c115b4ea0ecc867988
Johan Mosquerae <kira1974.dev@gmail.com>
2025-05-24 15:58:54 -0500

    Endpoint real del proyecto.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import paho.mqtt.client as mqtt
 import ssl
 import time
 import json
-
-
+import os
+import threading
+import requests
 
 app = Flask(__name__)
 
@@ -12,6 +13,36 @@ MQTT_BROKER = "d4abf07c55364762bf6b41af1122a4ab.s1.eu.hivemq.cloud"
 MQTT_PORT = 8883
 MQTT_USER = "johan"
 MQTT_PASSWORD = "Johan123."
+DJANGO_ENDPOINT = os.getenv(
+    "DJANGO_ENDPOINT",
+    "https://desarrollo-aquasmart-backend-develop.onrender.com/api/esp32/recibir-consumo"
+)
+# --- MQTT CALLBACK PARA DATOS ---
+def on_connect(client, userdata, flags, rc):
+    print("✅ Conectado al broker MQTT para recibir datos")
+    client.subscribe("caudal/lote/1852896-025/datos")
+
+
+def on_message(client, userdata, msg):
+    print(f"📩 Mensaje recibido en {msg.topic}:", msg.payload.decode())
+    try:
+        payload = json.loads(msg.payload.decode())
+        requests.post(DJANGO_ENDPOINT, json=payload)
+    except Exception as e:
+        print("❌ Error al reenviar a Django:", e)
+
+
+def start_mqtt_listener():
+    client = mqtt.Client()
+    client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    client.tls_set(cert_reqs=ssl.CERT_NONE)
+    client.tls_insecure_set(True)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(MQTT_BROKER, MQTT_PORT)
+    client.loop_forever()
+
 
 @app.route('/publicar_comando', methods=['POST'])
 def publicar_comando():
@@ -19,7 +50,6 @@ def publicar_comando():
     comando = data.get("comando")
     angulo = data.get("angulo")
     caudal = data.get("caudal")
-
 
     topic = f"caudal/lote/1852896-025/comandos"
     payload = {"comando": comando}
@@ -44,5 +74,7 @@ def publicar_comando():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    threading.Thread(target=start_mqtt_listener, daemon=True).start()
+    app.run(host='0.0.0.0', port=8000)  
